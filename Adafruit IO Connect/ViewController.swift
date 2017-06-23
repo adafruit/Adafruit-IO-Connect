ea15eaf3e6504a400445f08c78206c5488adb832
TrevKnows <Trevor.Beaton11@gmail.com>
2017-06-23 10:35:13 -0400

    Table Views in place

--- Adafruit IO Connect/ViewController.swift
+++ Adafruit IO Connect/ViewController.swift
@@ -8,18 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        identities = ["Accelerometer","Gyroscope", "Barometer"]
     }
 
+//In each view controller, we need to had different sensors to post
+    //Also, we have a textfield that will have a appended strign of the POST URL endpoint so that the data can be sent straight to Adafruit IO/feed_Name
+    //The first View -> Navigate or choose between the sensors you'll choose to be sent to your io feed
+    //This View Controller is for you to choose a sensor
+    //The first VC Accelerometer- contains A-IO Key, values that will be displayed in the current view.
+    
+ var sensors = ["Acclerometer Sensor","Barometer Sensor","Gyroscope Sensor"]
+    
+    var identities = [String]()
+   
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sensors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = sensors[indexPath.row]
+        
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vcNames = identities [indexPath.row]
+        let viewController = storyboard?.instantiateViewController(withIdentifier: vcNames)
+        self.navigationController?.pushViewController(viewController!, animated: true)
+    }
 
 }
 
