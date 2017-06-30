e58b4f22581bc6329aa152f3b3536768913c1145
TrevKnows <Trevor.Beaton11@gmail.com>
2017-06-30 12:24:31 -0400

    (Barometer VC) working

--- Adafruit IO Connect/BaroViewController.swift
+++ Adafruit IO Connect/BaroViewController.swift
@@ -15,29 +15,59 @@ class BaroViewController: UIViewController {
     var motionManager = CMMotionManager()
     lazy var altimeter = CMAltimeter()
     
-    @IBOutlet weak var switchChangeP: UISwitch!
+    
+    @IBOutlet weak var altiSwitch: UISwitch!
+    @IBOutlet weak var pressureSwitch: UISwitch!
     @IBOutlet weak var pressureTag: UILabel!
-  
+    @IBOutlet weak var altiTag: UILabel!
     
     
     @IBAction func switchChanged(_ sender: UISwitch) {
         if (sender.isOn == true){
-            self.startAltimeter()
-        }else {
+            self.pressureSwitch.setOn(false, animated: true)
+            self.startAltitudeMeter()
+        }else if (sender.isOn == false) {
             self.stopAltimeter()
         }
     }
-    @IBOutlet weak var altiTag: UILabel!
+   
+   
     
+    
+    
+    @IBAction func pressureSwitch(_ sender: UISwitch) {
+        if (sender.isOn == true){
+            self.altiSwitch.setOn(false, animated: true)
+            self.startPressureMeter()
+        }else {
+            self.stopPressureMeter()
+        }
+    }
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        startAltimeter()
+        self.altiTag.text = "--"
+        self.pressureTag.text = "--"
         
     }
 
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewWillDisappear(_ animated: Bool) {
         stopAltimeter()
+        stopPressureMeter()
     }
     
     func startAltimeter() {
@@ -67,16 +97,74 @@ class BaroViewController: UIViewController {
         }
     }
 
+    //new functions for individual sensors
+    func startAltitudeMeter() {
+        print("Sensor: Meters of relative altitude")
+        if(CMAltimeter.isRelativeAltitudeAvailable()) {
+            self.altimeter.startRelativeAltitudeUpdates(to: OperationQueue.main, withHandler: {
+                
+                (altimeterData: CMAltitudeData?, error: Error?) in
+                if (error != nil) {
+                    let alertView = UIAlertView(title: "Error", message: error!.localizedDescription, delegate: nil, cancelButtonTitle: "OK")
+                    alertView.show()
+                    
+                } else {
+                    
+                    let altitude = altimeterData?.relativeAltitude.floatValue
+               
+                    self.altiTag.text = String(format: "%0.2f", altitude!)
+                    print("Meters of relative Altitude: \(altitude ?? 0)")
+                
+                
+                    
+/*Altitude events reflect the change in the current altitude, not the absolute altitude. So a hiking app might use this object to track the user’s elevation gain over the course of a hike. Because altitude events may not be available on all devices, always call the isRelativeAltitudeAvailable() method before using this service. */
+                
+                }
+            })
+        }
+    }
+
+        func startPressureMeter() {
+            print("Sensor: Kilopascal of pressure")
+                if(CMAltimeter.isRelativeAltitudeAvailable()) {
+                    self.altimeter.startRelativeAltitudeUpdates(to: OperationQueue.main, withHandler: {
+                
+                (altimeterData: CMAltitudeData?, error: Error?) in
+                if (error != nil) {
+                    let alertView = UIAlertView(title: "Error", message: error!.localizedDescription, delegate: nil, cancelButtonTitle: "OK")
+                    alertView.show()
+                    
+                } else {
+                    
+                    let pressure = altimeterData?.pressure.floatValue
+                    
+                    self.pressureTag.text = String(format: "%0.2f", pressure!)
+                    print("Kilopascals of Pressure: \(pressure ?? 0)")
+               
+                //The recorded pressure, in kilopascals.
+                    
+                    
+                }
+            })
+        }
+    }
+    
+    
+    
+    
+    
+    func stopPressureMeter() {
+        self.pressureTag.text = "--"
+        self.altimeter.stopRelativeAltitudeUpdates() // Stop Update
+        print("Stopped Updates For Pressure Meter")
+
+    }
+    
     
     func stopAltimeter() {
-        
-        // Reset labels
         self.altiTag.text = "--"
-        self.pressureTag.text = "--"
-        
-        self.altimeter.stopRelativeAltitudeUpdates() // Stop updates
-        
-        print("Stopped Altitude Updates...")
+        self.altimeter.stopRelativeAltitudeUpdates() // Stop Update
+        print("Stopped Updates For Altitude Meter")
         
     }
 
--- Adafruit IO Connect/GyroViewController.swift
+++ Adafruit IO Connect/GyroViewController.swift
@@ -10,13 +10,17 @@ import UIKit
 import CoreMotion
 
 class GyroViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        startGyro()
+        
+        self.gyroTagX.text = "--"
+        self.gyroTagY.text = "--"
+        self.gyroTagZ.text = "--"
+        
         // Do any additional setup after loading the view.
     }
-
+    
     @IBOutlet weak var gyroTagZ: UILabel!
     @IBOutlet weak var gyroTagY: UILabel!
     @IBOutlet weak var gyroTagX: UILabel!
@@ -27,9 +31,9 @@ class GyroViewController: UIViewController {
         }else {
             self.stopGyro()
         }
-
+        
     }
-
+    
     //Data
     var motionManager = CMMotionManager()
     
@@ -40,7 +44,7 @@ class GyroViewController: UIViewController {
     
     
     func startGyro() {
-       print("Start Gyroscope Updates")
+        print("Start Gyroscope Updates")
         motionManager.gyroUpdateInterval = 2.2
         motionManager.startGyroUpdates(to: OperationQueue.main, withHandler: {
             (gyroData:CMGyroData?, error: Error?) in
@@ -54,20 +58,62 @@ class GyroViewController: UIViewController {
                 let gyroscopeY = gyroData?.rotationRate.y
                 let gyroscopeZ = gyroData?.rotationRate.z
                 
-                self.gyroTagX.text = String(format: "%.02f", gyroscopeX!)
-                self.gyroTagY.text = String(format: "%.02f", gyroscopeY!)
-                self.gyroTagZ.text = String(format: "%.02f", gyroscopeZ!)
+                self.gyroTagX.text = String(format: "%.2f", gyroscopeX!)
+                self.gyroTagY.text = String(format: "%.2f", gyroscopeY!)
+                self.gyroTagZ.text = String(format: "%.2f", gyroscopeZ!)
                 
                 
                 print("Gyroscope X:\(gyroscopeX!)")
                 print("Gyroscope Y:\(gyroscopeY!)")
                 print("Gyroscope Z:\(gyroscopeZ!)")
-                
+                self.postGyroData()
             }
         })
         
     }
-
+    
+    
+    func postGyroData() {
+        
+        // adafruit post syntax:
+        //http://io.adafruit.com/api/feeds/your-feed-key/data.json?X-AIO-Key=ed0dcb344edf621e39678f08533a674a197c5b75
+        
+        
+        let parameters = ["value": "\(String(format: "%.2f", (motionManager.gyroData?.rotationRate.x)!))"]
+        
+        //Create new Data //POST	/{username}/feeds/{feed_key}/data
+        //My AIO Key: c04d002a910e4eff85e6b83203d4e287
+        
+        guard let url = URL(string: "https://io.adafruit.com/api/feeds/text-feed/data.json?X-AIO-Key=c04d002a910e4eff85e6b83203d4e287") else { return }
+        
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "POST"
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else { return }
+        
+        request.httpBody = httpBody
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                }
+            }
+            
+            }.resume()
+    }
     
     
     
